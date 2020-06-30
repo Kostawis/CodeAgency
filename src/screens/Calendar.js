b73d5749e3f8f437b36c01ed8ba848b07bd28f89
Jakub <jakub.kolewczynski@gmail.com>
2020-06-30 16:52:16 +0200

    add calendar. add simpler display.

--- src/screens/Calendar.js
+++ src/screens/Calendar.js
@@ -1,50 +1,131 @@
-import React, {useState, useEffect} from 'react';
-import {Text, View, Button, StyleSheet} from 'react-native';
+import React, {Component} from 'react';
+import {StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+import {Agenda} from 'react-native-calendars';
+import {queryAll} from '../database/realmSchema';
+import moment from 'moment';
 
-const Calendar = ({navigation}) => {
-  // const [query, setQuery] = useState([]);
+class Calendar extends Component {
+  constructor(props) {
+    super(props);
 
-  // const reloadData = async () => setQuery(await displayAll());
+    this.state = {
+      items: {},
+    };
+  }
 
-  // const addTestToDo = async () => {
-  //   const newObj = {
-  //     id: Date.now(),
-  //     value: Math.floor(Math.random() * 1000000001).toString(),
-  //   };
-  //   try {
-  //     await addNew(newObj);
-  //     reloadData();
-  //   } catch (err) {
-  //     console.log(`Error: ${err}`);
-  //   }
-  // };
+  render() {
+    return (
+      <Agenda
+        items={this.state.items}
+        loadItemsForMonth={this.loadItems.bind(this)}
+        // selected={}
+        renderItem={this.renderItem.bind(this)}
+        renderEmptyDate={this.renderEmptyDate.bind(this)}
+        rowHasChanged={this.rowHasChanged.bind(this)}
+        // markingType={'period'}
+        // markedDates={{
+        //    '2017-05-08': {textColor: '#43515c'},
+        //    '2017-05-09': {textColor: '#43515c'},
+        //    '2017-05-14': {startingDay: true, endingDay: true, color: 'blue'},
+        //    '2017-05-21': {startingDay: true, color: 'blue'},
+        //    '2017-05-22': {endingDay: true, color: 'gray'},
+        //    '2017-05-24': {startingDay: true, color: 'gray'},
+        //    '2017-05-25': {color: 'gray'},
+        //    '2017-05-26': {endingDay: true, color: 'gray'}}}
+        // monthFormat={'yyyy'}
+        // theme={{calendarBackground: 'red', agendaKnobColor: 'green'}}
+        //renderDay={(day, item) => (<Text>{day ? day.day: 'item'}</Text>)}
+        // hideExtraDays={false}
+      />
+    );
+  }
 
-  // const deleteAllToDo = async () => {
-  //   try {
-  //     await deleteAll();
-  //     reloadData();
-  //   } catch (err) {
-  //     console.log(`Error: ${err}`);
-  //   }
-  // };
+  async loadItems(day) {
+    console.log(day.timestamp);
+    // this.state.items = {};
+    const items = await queryAll();
 
-  return (
-    <View style={styles.container}>
-      <Button title="Add" />
-      {/* <Button title="Add" onPress={addTestToDo} />
-      <Button title="Delete all" onPress={deleteAllToDo} />
-      {query.map(({id, value}) => (
-        <Text key={id}>{value}</Text>
-      ))} */}
-    </View>
-  );
-};
+    // items.forEach(({creationDate, description}, index) => {
+    //   const strTime = moment(creationDate).format('YYYY-MM-DD');
+    //   !this.state.items[strTime] && (this.state.items[strTime] = []);
+    //   this.state.items[strTime].push({
+    //     name: description,
+    //     index,
+    //   });
+    // });
+    // const newItems = {...this.state.items};
+    // this.setState({
+    //   items: newItems,
+    // });
+
+    for (let i = -15; i < 85; i++) {
+      const time = day.timestamp + i * 24 * 60 * 60 * 1000;
+      const strTime = this.timeToString(time);
+      if (!this.state.items[strTime]) {
+        this.state.items[strTime] = [];
+        const currDateItems = items.reduce((total, item) => {
+          strTime === moment(item.creationDate).format('YYYY-MM-DD') &&
+            (total = [...total, item]);
+          return total;
+        }, []);
+
+        currDateItems.length > 0 &&
+          currDateItems.forEach(item => {
+            this.state.items[strTime].push(item);
+          });
+      }
+    }
+    const newItems = {};
+    Object.keys(this.state.items).forEach(key => {
+      newItems[key] = this.state.items[key];
+    });
+    this.setState({
+      items: newItems,
+    });
+  }
+
+  renderItem(item) {
+    return (
+      <TouchableOpacity
+        style={styles.item}
+        onPress={() => console.log(item.id)}>
+        <Text>{item.id}</Text>
+        <Text>{item.description}</Text>
+      </TouchableOpacity>
+    );
+  }
+
+  renderEmptyDate() {
+    return (
+      <View style={styles.emptyDate}>
+        <Text>This is empty date!</Text>
+      </View>
+    );
+  }
+
+  rowHasChanged(r1, r2) {
+    return r1.name !== r2.name;
+  }
+
+  timeToString(time) {
+    const date = new Date(time);
+    return date.toISOString().split('T')[0];
+  }
+}
 
 const styles = StyleSheet.create({
-  container: {
+  item: {
+    backgroundColor: 'white',
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    borderRadius: 5,
+    padding: 10,
+    marginRight: 10,
+    marginTop: 8,
+  },
+  emptyDate: {
+    height: 15,
+    flex: 1,
+    paddingTop: 30,
   },
 });
 
